a67d68cc3827a5af5a9820d2df313e9a2f947b4a
Marcel Wantia <mwantia@outlook.de>
2025-02-05 11:48:17 +0000

    Anthropic now performs its tool calling within the 'Chat' function to separate internal logic from the external provider input/output

--- plugins/anthropic/anthropic.go
+++ plugins/anthropic/anthropic.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/liushuangls/go-anthropic/v2"
+	"github.com/liushuangls/go-anthropic/v2/jsonschema"
 	"github.com/mwantia/queueverse/pkg/plugin/provider"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -23,24 +24,83 @@ func (p *AnthropicProvider) GetModels() (*[]provider.Model, error) {
 }
 
 func (p *AnthropicProvider) Chat(input provider.ChatRequest) (*provider.ChatResponse, error) {
+	tools := make([]anthropic.ToolDefinition, 0)
+	for _, tool := range input.Tools {
+		properties := make(map[string]jsonschema.Definition, 0)
+		for name, property := range tool.Properties {
+			properties[name] = jsonschema.Definition{
+				Type:        jsonschema.DataType(property.Type),
+				Enum:        property.Enum,
+				Description: property.Description,
+			}
+		}
+
+		tools = append(tools, anthropic.ToolDefinition{
+			Name:        tool.Name,
+			Description: tool.Description,
+			InputSchema: jsonschema.Definition{
+				Type:       jsonschema.Object,
+				Required:   tool.Required,
+				Properties: properties,
+			},
+		})
+	}
+
 	request := anthropic.MessagesRequest{
-		Model: anthropic.Model(input.Model),
+		MaxTokens: 100,
+		Model:     anthropic.Model(input.Model),
 		Messages: []anthropic.Message{
 			anthropic.NewUserTextMessage(input.Message.Content),
 		},
+		Tools: tools,
 	}
 
-	response, err := p.Client.CreateMessages(p.Context, request)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create messages: %w", err)
+	max := 5
+	for attempts := 0; attempts < max; attempts++ {
+		response, err := p.Client.CreateMessages(p.Context, request)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create messages: %w", err)
+		}
+
+		request.Messages = append(request.Messages, anthropic.Message{
+			Role:    anthropic.RoleAssistant,
+			Content: response.Content,
+		})
+
+		var use *anthropic.MessageContentToolUse
+		for _, content := range response.Content {
+			if content.Type == anthropic.MessagesContentTypeToolUse {
+				use = content.MessageContentToolUse
+				break
+			}
+		}
+
+		if use == nil {
+			return &provider.ChatResponse{
+				Model: string(response.Model),
+				Message: provider.Message{
+					Content: response.Content[0].GetText(),
+				},
+			}, nil
+		}
+
+		function := provider.ToolFunction{
+			Index: 0,
+			Name:  use.Name,
+		}
+		if err := use.UnmarshalInput(&function.Arguments); err != nil {
+			return nil, err
+		}
+
+		result, err := input.Handler(p.Context, function)
+		if err != nil {
+			return nil, err
+		}
+
+		request.Messages = append(request.Messages, anthropic.NewToolResultsMessage(use.ID, result, false))
 	}
 
-	return &provider.ChatResponse{
-		Model: string(response.Model),
-		Message: provider.Message{
-			Content: response.Content[0].GetText(),
-		},
-	}, nil
+	return nil, fmt.Errorf("max attempts reached")
 }
 
 func (*AnthropicProvider) Embed(provider.EmbedRequest) (*provider.EmbedResponse, error) {
